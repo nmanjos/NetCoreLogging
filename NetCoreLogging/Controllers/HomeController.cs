8e04abf60323f5cc2556f0f1976ea7e2b4626a87
Nuno Miguel Pereira Mendes dos Anjos <nanjos@live.com>
2019-06-08 08:13:16 +0100

    Add project files.

---
+++ NetCoreLogging/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreLogging.Models;
+
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreLogging.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger Logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            Logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Logger.LogInformation("Index on home controller");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NetCoreLogging/Logger/FileLoggerOptions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreLogging
+{
+    public class FileLoggerOptions
+    {
+        string fFolder;
+        int fMaxFileSizeInMB;
+        int fRetainPolicyFileCount;
+
+        public FileLoggerOptions()
+        {
+        }
+
+        public LogLevel LogLevel { get; set; } = Microsoft.Extensions.Logging.LogLevel.Information;
+
+        public string Folder
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(fFolder) ?
+                fFolder : System.IO.Path.GetDirectoryName(this.GetType().Assembly.Location);
+            }
+            set { fFolder = value; }
+        }
+
+        public int MaxFileSizeInMB
+        {
+            get { return fMaxFileSizeInMB > 0 ? fMaxFileSizeInMB : 2; }
+            set { fMaxFileSizeInMB = value; }
+        }
+
+        public int RetainPolicyFileCount
+        {
+            get { return fRetainPolicyFileCount < 5 ? 5 : fRetainPolicyFileCount; }
+            set { fRetainPolicyFileCount = value; }
+        }
+    }
+}
